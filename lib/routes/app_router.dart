9b50200f9edc19271aab5d13707ed61139f227d5
Joel Magoun <joel.magoun@gmail.com>
2025-01-10 11:00:11 +0000

    2.3.2.24 - improve state management on task details

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -45,7 +45,7 @@ class AppRouter {
       case Routes.goals:
         return const GoalsScreen();
       case Routes.taskDetails:
-        return TaskDetailsScreen(task: settings.arguments as Task);
+        return TaskDetailsScreen(initialTask: settings.arguments as Task);
       case Routes.goalDetails:
         return GoalDetailsScreen(goal: settings.arguments as Goal);
       case Routes.profile:
